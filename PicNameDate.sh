41564dc44b96a5a348151aa0f404dc74f14d9795
Nate Rauh <NRauh@users.noreply.github.com>
2015-02-27 20:03:30 -0600

    Fixed problem with file names
    Script was treating filenames with spaces as seperate arguments.
    Fixed that, which also (I think) fixed problems having other non-alphanumeric characters

--- PicNameDate.sh
+++ PicNameDate.sh
@@ -1,37 +1,36 @@
 #!/bin/bash
 
 changeFile() {
-	if ! exiftool $1 -mimetype -S -s | grep -q "image"
+	if ! exiftool "$1" -mimetype -S -s | grep -q "image"
 	then
 		return
 	fi
-	PIC=$1
+	PIC="$1"
 	EXT="${PIC##*.}"
-	DATE="`exiftool $PIC -d "%Y-%m-%d_%H-%M-%S" -datetimeoriginal -S -s`"
+	DATE="`exiftool "$PIC" -d "%Y-%m-%d_%H-%M-%S" -datetimeoriginal -S -s`"
 	NEWNAME="${DATE}.${EXT}"
 
 	check() {
-		if [ -e $NEWNAME ]; then
-			NEWNAME=$DATE"_"$1.$EXT
+		if [ -e "$NEWNAME" ]; then
+			NEWNAME="${DATE}_${1}.${EXT}"
 			check $(($1+1))
 		fi
 	}
 
 	check 1
 
-	cp -np $PIC $BUDIR/$PIC
-	mv -nv $PIC $NEWNAME
+	cp -np "$PIC" "$BUDIR/$PIC"
+	mv -nv "$PIC" "$NEWNAME"
 	echo "$NEWNAME:" >> Descriptions.txt
 }
 
 BUDIR="pnd-backup-`date +"%s"`"
 
 if [ -d $1 ]; then
-	echo "Will do for directory"
 	cd $1
 	mkdir $BUDIR
-	find . -maxdepth 1 -type f | while read file; do
-		changeFile $file
+	find . -maxdepth 1 -type f -print0 | while read -r -d '' file; do
+		changeFile "$file"
 	done
 elif [ -e $1 ]; then
 	cd `dirname $1`
@@ -41,5 +40,6 @@ else
 	echo "Need"
 	# Will not work with slashes in path
 	# Usage PicNameDate.sh path
+	# Dropbox names have _1 for some reason
 fi
 
